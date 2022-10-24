171b3f80b89bc6afa2650f9ab8290c7c86fe54a8
Arkadiusz Baraniecki <arkadiusz.baraniecki@allegro.pl>
2022-10-24 21:19:37 +0200

    Parameterization of the Test2.test3

--- examples/testNG-single-module/src/test/java/Test2.java
+++ examples/testNG-single-module/src/test/java/Test2.java
@@ -1,3 +1,5 @@
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 import static org.testng.AssertJUnit.assertTrue;
@@ -7,18 +9,29 @@ public class Test2 {
 
     private static final int sleepDurationMs = 300;
 
+    @DataProvider(name = "data-provider", parallel = true)
+    public static Object[][] createData() {
+        return new Object[][]{{1}, {2}};
+    }
+
+
+    @Test
     public void test1() throws InterruptedException {
         Thread.sleep(sleepDurationMs);
         assertTrue(true);
     }
 
+    @Ignore
+    @Test
     public void test2() throws InterruptedException {
         Thread.sleep(sleepDurationMs);
         assertTrue(true);
     }
 
-    public void test3() throws InterruptedException {
+    @Test(dataProvider = "data-provider")
+    public void test3(int x) throws InterruptedException {
         Thread.sleep(sleepDurationMs);
+        System.out.println("x = " + x);
         assertTrue(true);
     }
 }
