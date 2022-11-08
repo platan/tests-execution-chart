90626acd9317a8074cab3fe9a7bf7f03128c8fe8
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2022-11-08 18:36:14 +0100

    Rename examples to test-projects

--- .readme/prepare_example_charts.sh
+++ .readme/prepare_example_charts.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-npx -p @mermaid-js/mermaid-cli mmdc -i ../examples/spock-single-module/build/reports/tests-execution/mermaid/test.txt -o example_dark.svg -t dark -b transparent
-npx -p @mermaid-js/mermaid-cli mmdc -i ../examples/spock-single-module/build/reports/tests-execution/mermaid/test.txt -o example.svg -b transparent
+npx -p @mermaid-js/mermaid-cli mmdc -i ../test-projects/spock-single-module/build/reports/tests-execution/mermaid/test.txt -o example_dark.svg -t dark -b transparent
+npx -p @mermaid-js/mermaid-cli mmdc -i ../test-projects/spock-single-module/build/reports/tests-execution/mermaid/test.txt -o example.svg -b transparent
