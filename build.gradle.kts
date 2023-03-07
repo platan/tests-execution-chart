a4fd151509feff471f47238357b7f83317492fc7
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2023-03-07 19:09:52 +0100

    Use 0.2.1 in project

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("com.gradle.plugin-publish") version "1.1.0"
     id("com.diffplug.spotless") version "6.16.0"
     id("com.github.jakemarsden.git-hooks") version "0.0.2"
-    id("io.github.platan.tests-execution-chart") version "0.2.0"
+    id("io.github.platan.tests-execution-chart") version "0.2.1"
 }
 
 group = "io.github.platan"
