4bcea28d78f200238d405ee69443537c14bf60e6
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2023-03-20 19:23:43 +0100

    Extract port

--- src/visual-regression/tests/report.spec.ts
+++ src/visual-regression/tests/report.spec.ts
@@ -3,17 +3,18 @@ import percySnapshot from '@percy/playwright';
 import handler from 'serve-handler';
 import http from 'http';
 
+const port = 3000;
 test.beforeEach(async ({ page }) => {
   const server = http.createServer((request, response) => {
     return handler(request, response, { public: '../../build/html-report' });
   });
 
-  server.listen(3000);
+  server.listen(port);
 });
 
 
 test('open the report and take a screenshot', async ({ page }) => {
-  await page.goto('http://localhost:3000/example-task.html');
+  await page.goto(`http://localhost:${port}/example-task.html`);
 
   await expect(page).toHaveTitle(/Test execution chart/);
   await percySnapshot(page, 'Test execution chart page');
