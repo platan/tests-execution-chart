a375262fecd4ef995e28dd8b1b8a01c104d666a0
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2023-03-28 21:04:03 +0200

    Update groovy

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ dependencies {
     implementation("org.apache.commons:commons-text:1.10.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
 
-    testImplementation(platform("org.codehaus.groovy:groovy-bom:3.0.15"))
+    testImplementation(platform("org.codehaus.groovy:groovy-bom:3.0.16"))
     testImplementation("org.codehaus.groovy:groovy")
     testImplementation(platform("org.spockframework:spock-bom:2.3-groovy-3.0"))
     testImplementation("org.spockframework:spock-core")
