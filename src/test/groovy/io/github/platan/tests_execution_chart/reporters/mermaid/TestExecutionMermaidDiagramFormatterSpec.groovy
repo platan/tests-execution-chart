bdeebae5314ae4386ea4925d6d37bd5035724931
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2023-04-19 22:09:54 +0200

    Set time zone in tests

--- src/test/groovy/io/github/platan/tests_execution_chart/reporters/mermaid/TestExecutionMermaidDiagramFormatterSpec.groovy
+++ src/test/groovy/io/github/platan/tests_execution_chart/reporters/mermaid/TestExecutionMermaidDiagramFormatterSpec.groovy
@@ -1,13 +1,16 @@
 package io.github.platan.tests_execution_chart.reporters.mermaid
 
-
 import io.github.platan.tests_execution_chart.report.TestExecutionScheduleReportBuilder
 import spock.lang.Specification
+
+import java.time.ZoneOffset
 
 class TestExecutionMermaidDiagramFormatterSpec extends Specification {
 
     def "should format report"() {
         given:
+        def getDefault = TimeZone.getDefault()
+        TimeZone.setDefault(SimpleTimeZone.getTimeZone(ZoneOffset.ofHours(2)))
         def reportBuilder = new TestExecutionScheduleReportBuilder()
         reportBuilder.add('Test1', 'test1', 1681402397000, 1681402397100, 'SUCCESS')
         reportBuilder.add('Test1', 'test2', 1681402397100, 1681402397300, 'SUCCESS')
@@ -28,5 +31,8 @@ test1 - 100 ms :active, 2023-04-13T18:13:17.000+0200, 2023-04-13T18:13:17.100+02
 """
         then:
         mermaidReport == indent
+
+        cleanup:
+        TimeZone.setDefault(getDefault)
     }
 }
