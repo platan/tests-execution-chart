055d4226fce309645590bd502a77f7d7bf16fd00
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2023-04-19 21:47:03 +0200

    Add TestExecutionMermaidDiagramFormatterSpec

---
+++ src/test/groovy/io/github/platan/tests_execution_chart/reporters/mermaid/TestExecutionMermaidDiagramFormatterSpec.groovy
@@ -0,0 +1,32 @@
+package io.github.platan.tests_execution_chart.reporters.mermaid
+
+
+import io.github.platan.tests_execution_chart.report.TestExecutionScheduleReportBuilder
+import spock.lang.Specification
+
+class TestExecutionMermaidDiagramFormatterSpec extends Specification {
+
+    def "should format report"() {
+        given:
+        def reportBuilder = new TestExecutionScheduleReportBuilder()
+        reportBuilder.add('Test1', 'test1', 1681402397000, 1681402397100, 'SUCCESS')
+        reportBuilder.add('Test1', 'test2', 1681402397100, 1681402397300, 'SUCCESS')
+        reportBuilder.add('Test2', 'test1', 1681402397000, 1681402397100, 'SUCCESS')
+
+        when:
+        def mermaidReport = new TestExecutionMermaidDiagramFormatter().format(reportBuilder.getResults())
+
+
+        def indent = """gantt
+dateFormat YYYY-MM-DDTHH:mm:ss.SSSZZ
+axisFormat %H:%M:%S.%L
+section Test1
+test1 - 100 ms :active, 2023-04-13T18:13:17.000+0200, 2023-04-13T18:13:17.100+0200
+test2 - 200 ms :active, 2023-04-13T18:13:17.100+0200, 2023-04-13T18:13:17.300+0200
+section Test2
+test1 - 100 ms :active, 2023-04-13T18:13:17.000+0200, 2023-04-13T18:13:17.100+0200
+"""
+        then:
+        mermaidReport == indent
+    }
+}
