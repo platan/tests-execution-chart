a45a76d5ef44157dfb0b76d0d13c3d52f7d5b2d9
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2023-05-24 17:53:08 +0200

    Remove unused param

--- src/main/kotlin/io/github/platan/tests_execution_chart/config/Marks.kt
+++ src/main/kotlin/io/github/platan/tests_execution_chart/config/Marks.kt
@@ -1,11 +1,9 @@
 package io.github.platan.tests_execution_chart.config
 
 import org.gradle.api.Action
-import org.gradle.api.model.ObjectFactory
 import org.gradle.api.tasks.Nested
-import javax.inject.Inject
 
-abstract class Marks @Inject constructor(objectFactory: ObjectFactory) {
+abstract class Marks {
 
     @Nested
     abstract fun getTotalTimeOfAllTests(): Mark
