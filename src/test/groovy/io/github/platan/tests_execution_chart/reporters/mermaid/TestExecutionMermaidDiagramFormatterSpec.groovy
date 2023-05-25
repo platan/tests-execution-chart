23869d4af8ffd4b810f56d5cbdd18d55168b6147
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2023-05-25 21:06:09 +0200

    Add test for TestExecutionMermaidDiagramFormatter without marks

--- src/test/groovy/io/github/platan/tests_execution_chart/reporters/mermaid/TestExecutionMermaidDiagramFormatterSpec.groovy
+++ src/test/groovy/io/github/platan/tests_execution_chart/reporters/mermaid/TestExecutionMermaidDiagramFormatterSpec.groovy
@@ -8,7 +8,37 @@ import java.time.ZoneOffset
 
 class TestExecutionMermaidDiagramFormatterSpec extends Specification {
 
-    def "should format report"() {
+    def "should format report with test results"() {
+        given:
+        def getDefault = TimeZone.getDefault()
+        TimeZone.setDefault(SimpleTimeZone.getTimeZone(ZoneOffset.ofHours(2)))
+        def reportBuilder = new TestExecutionScheduleReportBuilder()
+        reportBuilder.add('Test1', 'test1', 1681402397000, 1681402397100, 'SUCCESS')
+        reportBuilder.add('Test1', 'test2', 1681402397100, 1681402397300, 'SUCCESS')
+        reportBuilder.add('Test2', 'test1', 1681402397000, 1681402397100, 'SUCCESS')
+        def report = reportBuilder.getResults()
+
+        when:
+        def mermaidReport = new TestExecutionMermaidDiagramFormatter().format(report)
+
+
+        def indent = """gantt
+dateFormat YYYY-MM-DDTHH:mm:ss.SSSZZ
+axisFormat %H:%M:%S.%L
+section Test1
+test1 - 100 ms :active, 2023-04-13T18:13:17.000+0200, 2023-04-13T18:13:17.100+0200
+test2 - 200 ms :active, 2023-04-13T18:13:17.100+0200, 2023-04-13T18:13:17.300+0200
+section Test2
+test1 - 100 ms :active, 2023-04-13T18:13:17.000+0200, 2023-04-13T18:13:17.100+0200
+"""
+        then:
+        mermaidReport == indent
+
+        cleanup:
+        TimeZone.setDefault(getDefault)
+    }
+
+    def "should format report with test results and marks"() {
         given:
         def getDefault = TimeZone.getDefault()
         TimeZone.setDefault(SimpleTimeZone.getTimeZone(ZoneOffset.ofHours(2)))
