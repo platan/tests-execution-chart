785dc32a09c737de63cf2136c7dc3c434f5272ec
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2023-10-16 21:26:23 +0200

    Move default script src to Script

--- src/main/kotlin/io/github/platan/tests_execution_chart/gradle/config/formats/Script.kt
+++ src/main/kotlin/io/github/platan/tests_execution_chart/gradle/config/formats/Script.kt
@@ -1,5 +1,6 @@
 package io.github.platan.tests_execution_chart.gradle.config.formats
 
+import io.github.platan.tests_execution_chart.reporters.html.HtmlConfig
 import org.gradle.api.Action
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.Property
@@ -7,12 +8,10 @@ import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Nested
 import javax.inject.Inject
 
-const val DEFAULT_MERMAID_SCRIPT_SRC = "https://cdn.jsdelivr.net/npm/mermaid@10.5.0/dist/mermaid.min.js"
-
 abstract class Script @Inject constructor(objectFactory: ObjectFactory) {
 
     @get:Input
-    val src: Property<String> = objectFactory.property(String::class.java).convention(DEFAULT_MERMAID_SCRIPT_SRC)
+    val src: Property<String> = objectFactory.property(String::class.java).convention(HtmlConfig.Script.getSrcDefault())
 
     @get:Input
     val embed: Property<Boolean> = objectFactory.property(Boolean::class.java).convention(false)
--- tests-execution-chart-commons/src/main/kotlin/io/github/platan/tests_execution_chart/reporters/html/HtmlConfig.kt
+++ tests-execution-chart-commons/src/main/kotlin/io/github/platan/tests_execution_chart/reporters/html/HtmlConfig.kt
@@ -5,6 +5,14 @@ import io.github.platan.tests_execution_chart.report.ReportConfig
 data class HtmlConfig(override val enabled: Boolean, override val outputLocation: String, val script: Script) :
     ReportConfig.Format {
     data class Script(val src: String, val embed: Boolean, val options: Options) {
+
+        companion object {
+            private const val DEFAULT_MERMAID_SCRIPT_SRC =
+                "https://cdn.jsdelivr.net/npm/mermaid@10.5.0/dist/mermaid.min.js"
+
+            fun getSrcDefault(): String = DEFAULT_MERMAID_SCRIPT_SRC
+        }
+
         data class Options(val maxTextSize: Int)
     }
 }
